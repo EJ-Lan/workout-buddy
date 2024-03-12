3c417bcbaf01d6ccccfe43cb6eb2429bf0f12ab9
EJ-Lan <ervinjon_lanada@outlook.com>
2024-03-12 08:22:17 -0600

    Add some routes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
-const express = require('express');
 require('dotenv').config()
+
+const express = require('express');
+const workoutRoutes = require('./routes/workouts');
 
 // express app
 const app = express()
@@ -10,10 +12,8 @@ app.use((req, res, next) => {
     next()
 })
 
-// routes
-app.get('/', (req, res) => {
-    res.json({mssg: 'Welcome to the app'})
-})
+//routes
+app.use('/api/workouts', workoutRoutes)
 
 // listen for requests
 app.listen(process.env.PORT, () => {

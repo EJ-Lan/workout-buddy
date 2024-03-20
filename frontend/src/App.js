5ff29061b2a08b2eb7b1f64ecb3e16d6aa25b5cf
Ervin Lanada <ervinjon_lanada@outlook.com>
2024-03-20 03:20:52 -0600

    Context added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,18 +4,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Navbar from './components/Navbar'
 
-
 function App() {
+
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
-
-        <div className="page">
+        <div className="pages">
           <Routes>
             <Route 
-              path="/"
-              element={<Home />}
+              path="/" 
+              element={<Home />} 
             />
           </Routes>
         </div>
